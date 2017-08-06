72e01eb43c4755a931e5f71bca0c0189f74ead15
Liza <Liza@192.168.1.102>
2017-08-06 14:45:23 +0200

    created board with the single rectangles x 64

--- src/kamisado_client/Kamisado_Client_View.java
+++ src/kamisado_client/Kamisado_Client_View.java
@@ -1,13 +1,16 @@
 package kamisado_client;
 
-import javafx.scene.Scene;
 import java.util.logging.Logger;
+
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class Kamisado_Client_View {
@@ -16,17 +19,32 @@ public class Kamisado_Client_View {
 	private Kamisado_Client_Model model;
 	protected Button btnClick;
 	
+	public static final int FIELD_SIZE = 70;
+	public static final int WIDTH = 8;
+	public static final int HEIGHT = 8;
+//	private Rectangle field;
+	
+		
+	// creates new field for the board
+	/*public Rectangle getField (Rectangle field) {
+		this.field = new Rectangle();
+		field.setWidth(FIELD_SIZE);
+		field.setHeight(FIELD_SIZE);
+		field.setFill(Color.BLUE);
+		return field;
+	}*/
 	protected Kamisado_Client_View(Stage stage, Kamisado_Client_Model model){
 		this.stage = stage;
 		this.model = model;
 		
+		
+	
 		//View Stuff goes here
 		btnClick = new Button(); //Only an example for showing in the controller how to register for events
 		
 		BorderPane root = new BorderPane();
 		GridPane gameBoard = new GridPane(); // Kamisado game board
-		root.setCenter(gameBoard);
-		
+	
 		HBox upperScreen = new HBox(); // upper part of the game screen for buttons
 		root.setTop(upperScreen); 
 		Button resetGame = new Button("ResetGame");
@@ -36,12 +54,428 @@ public class Kamisado_Client_View {
 		Label moveWait = new Label("Move / Wait");
 		upperScreen.getChildren().addAll(resetGame, giveUp, moveWait, language);
 		
-		for (int row = 0; row < 8; row++){
-			for (int col = 0; col < 8; col++){
-				Label lblTemp = new Label(row + " , " + col);
-				gameBoard.add(lblTemp, col, row);
+		
+		
+		
+		/*
+		for (int row = 0; row < WIDTH; row++){
+			for (int col = 0; col < HEIGHT; col++){
+			Rectangle field = new Rectangle();
+				field.setWidth(FIELD_SIZE);
+				field.setHeight(FIELD_SIZE);
+				field.setStroke(Color.BEIGE);
+				
+				gameBoard.add(field, row, col);
 			}
-		}
+			*/
+		// first row
+		Rectangle a = new Rectangle();
+		a.setWidth(FIELD_SIZE);
+		a.setHeight(FIELD_SIZE);
+		a.setFill(Color.ORANGE);
+		gameBoard.add(a, 0, 0);
+		
+		Rectangle b = new Rectangle();
+		b.setWidth(FIELD_SIZE);
+		b.setHeight(FIELD_SIZE);
+		b.setFill(Color.BLUE);
+		gameBoard.add(b, 1, 0);
+			
+		Rectangle c = new Rectangle();
+		c.setWidth(FIELD_SIZE);
+		c.setHeight(FIELD_SIZE);
+		c.setFill(Color.PURPLE);
+		gameBoard.add(c, 2, 0);
+		
+		Rectangle d = new Rectangle();
+		d.setWidth(FIELD_SIZE);
+		d.setHeight(FIELD_SIZE);
+		d.setFill(Color.PINK);
+		gameBoard.add(d, 3, 0);
+		
+		Rectangle e = new Rectangle();
+		e.setWidth(FIELD_SIZE);
+		e.setHeight(FIELD_SIZE);
+		e.setFill(Color.YELLOW);
+		gameBoard.add(e, 4, 0);
+		
+		Rectangle f = new Rectangle();
+		f.setWidth(FIELD_SIZE);
+		f.setHeight(FIELD_SIZE);
+		f.setFill(Color.RED);
+		gameBoard.add(f, 5, 0);
+		
+		
+		Rectangle g = new Rectangle();
+		g.setWidth(FIELD_SIZE);
+		g.setHeight(FIELD_SIZE);
+		g.setFill(Color.GREEN);
+		gameBoard.add(g, 6, 0);
+		
+		
+		Rectangle h = new Rectangle();
+		h.setWidth(FIELD_SIZE);
+		h.setHeight(FIELD_SIZE);
+		h.setFill(Color.BROWN);
+		gameBoard.add(h, 7, 0);
+		
+		// second  row
+		Rectangle i = new Rectangle();
+		i.setWidth(FIELD_SIZE);
+		i.setHeight(FIELD_SIZE);
+		i.setFill(Color.RED);
+		gameBoard.add(i, 0, 1);
+			
+		Rectangle j = new Rectangle();
+		j.setWidth(FIELD_SIZE);
+		j.setHeight(FIELD_SIZE);
+		j.setFill(Color.ORANGE);
+		gameBoard.add(j, 1, 1);
+		
+		Rectangle k = new Rectangle();
+		k.setWidth(FIELD_SIZE);
+		k.setHeight(FIELD_SIZE);
+		k.setFill(Color.PINK);
+		gameBoard.add(k, 2, 1);
+		
+		Rectangle l = new Rectangle();
+		l.setWidth(FIELD_SIZE);
+		l.setHeight(FIELD_SIZE);
+		l.setFill(Color.GREEN);
+		gameBoard.add(l, 3, 1);
+		
+		Rectangle m = new Rectangle();
+		m.setWidth(FIELD_SIZE);
+		m.setHeight(FIELD_SIZE);
+		m.setFill(Color.BLUE);
+		gameBoard.add(m, 4, 1);
+		
+		
+		Rectangle n = new Rectangle();
+		n.setWidth(FIELD_SIZE);
+		n.setHeight(FIELD_SIZE);
+		n.setFill(Color.YELLOW);
+		gameBoard.add(n, 5, 1);
+				
+		Rectangle o = new Rectangle();
+		o.setWidth(FIELD_SIZE);
+		o.setHeight(FIELD_SIZE);
+		o.setFill(Color.BROWN);
+		gameBoard.add(o, 6, 1);
+		
+		Rectangle p = new Rectangle();
+		p.setWidth(FIELD_SIZE);
+		p.setHeight(FIELD_SIZE);
+		p.setFill(Color.PURPLE);
+		gameBoard.add(p, 7, 1);
+		
+		// THIRD ROW
+		Rectangle r = new Rectangle();
+		r.setWidth(FIELD_SIZE);
+		r.setHeight(FIELD_SIZE);
+		r.setFill(Color.GREEN);
+		gameBoard.add(r, 0, 2);
+			
+		Rectangle s = new Rectangle();
+		s.setWidth(FIELD_SIZE);
+		s.setHeight(FIELD_SIZE);
+		s.setFill(Color.PINK);
+		gameBoard.add(s, 1, 2);
+		
+		Rectangle t = new Rectangle();
+		t.setWidth(FIELD_SIZE);
+		t.setHeight(FIELD_SIZE);
+		t.setFill(Color.ORANGE);
+		gameBoard.add(t, 2, 2);
+		
+		Rectangle u = new Rectangle();
+		u.setWidth(FIELD_SIZE);
+		u.setHeight(FIELD_SIZE);
+		u.setFill(Color.RED);
+		gameBoard.add(u, 3, 2);
+		
+		Rectangle v = new Rectangle();
+		v.setWidth(FIELD_SIZE);
+		v.setHeight(FIELD_SIZE);
+		v.setFill(Color.PURPLE);
+		gameBoard.add(v, 4, 2);
+		
+		
+		Rectangle w = new Rectangle();
+		w.setWidth(FIELD_SIZE);
+		w.setHeight(FIELD_SIZE);
+		w.setFill(Color.BROWN);
+		gameBoard.add(w, 5, 2);
+				
+		Rectangle x = new Rectangle();
+		x.setWidth(FIELD_SIZE);
+		x.setHeight(FIELD_SIZE);
+		x.setFill(Color.YELLOW);
+		gameBoard.add(x, 6, 2);
+		
+		Rectangle y = new Rectangle();
+		y.setWidth(FIELD_SIZE);
+		y.setHeight(FIELD_SIZE);
+		y.setFill(Color.BLUE);
+		gameBoard.add(y, 7, 2);
+		
+		// fourth
+		
+		Rectangle aa = new Rectangle();
+		aa.setWidth(FIELD_SIZE);
+		aa.setHeight(FIELD_SIZE);
+		aa.setFill(Color.PINK);
+		gameBoard.add(aa, 0, 3);
+			
+		Rectangle bb = new Rectangle();
+		bb.setWidth(FIELD_SIZE);
+		bb.setHeight(FIELD_SIZE);
+		bb.setFill(Color.PURPLE);
+		gameBoard.add(bb, 1, 3);
+		
+		Rectangle cc = new Rectangle();
+		cc.setWidth(FIELD_SIZE);
+		cc.setHeight(FIELD_SIZE);
+		cc.setFill(Color.BLUE);
+		gameBoard.add(cc, 2, 3);
+		
+		Rectangle dd = new Rectangle();
+		dd.setWidth(FIELD_SIZE);
+		dd.setHeight(FIELD_SIZE);
+		dd.setFill(Color.ORANGE);
+		gameBoard.add(dd, 3, 3);
+		
+		Rectangle ee = new Rectangle();
+		ee.setWidth(FIELD_SIZE);
+		ee.setHeight(FIELD_SIZE);
+		ee.setFill(Color.BROWN);
+		gameBoard.add(ee, 4, 3);
+		
+		
+		Rectangle ff = new Rectangle();
+		ff.setWidth(FIELD_SIZE);
+		ff.setHeight(FIELD_SIZE);
+		ff.setFill(Color.GREEN);
+		gameBoard.add(ff, 5, 3);
+				
+		Rectangle gg = new Rectangle();
+		gg.setWidth(FIELD_SIZE);
+		gg.setHeight(FIELD_SIZE);
+		gg.setFill(Color.RED);
+		gameBoard.add(gg, 6, 3);
+		
+		Rectangle hh = new Rectangle();
+		hh.setWidth(FIELD_SIZE);
+		hh.setHeight(FIELD_SIZE);
+		hh.setFill(Color.YELLOW);
+		gameBoard.add(hh, 7, 3);
+		
+
+		
+		// eight row
+				Rectangle ii = new Rectangle();
+				ii.setWidth(FIELD_SIZE);
+				ii.setHeight(FIELD_SIZE);
+				ii.setFill(Color.ORANGE);
+				gameBoard.add(ii, 7, 7);
+				
+				Rectangle jj = new Rectangle();
+				jj.setWidth(FIELD_SIZE);
+				jj.setHeight(FIELD_SIZE);
+				jj.setFill(Color.BLUE);
+				gameBoard.add(jj, 6, 7);
+					
+				Rectangle kk = new Rectangle();
+				kk.setWidth(FIELD_SIZE);
+				kk.setHeight(FIELD_SIZE);
+				kk.setFill(Color.PURPLE);
+				gameBoard.add(kk, 5, 7);
+				
+				Rectangle ll = new Rectangle();
+				ll.setWidth(FIELD_SIZE);
+				ll.setHeight(FIELD_SIZE);
+				ll.setFill(Color.PINK);
+				gameBoard.add(ll, 4, 7);
+				
+				Rectangle mm = new Rectangle();
+				mm.setWidth(FIELD_SIZE);
+				mm.setHeight(FIELD_SIZE);
+				mm.setFill(Color.YELLOW);
+				gameBoard.add(mm, 3, 7);
+				
+				Rectangle nn = new Rectangle();
+				nn.setWidth(FIELD_SIZE);
+				nn.setHeight(FIELD_SIZE);
+				nn.setFill(Color.RED);
+				gameBoard.add(nn, 2, 7);
+				
+				
+				Rectangle oo = new Rectangle();
+				oo.setWidth(FIELD_SIZE);
+				oo.setHeight(FIELD_SIZE);
+				oo.setFill(Color.GREEN);
+				gameBoard.add(oo, 1, 7);
+				
+				
+				Rectangle pp = new Rectangle();
+				pp.setWidth(FIELD_SIZE);
+				pp.setHeight(FIELD_SIZE);
+				pp.setFill(Color.BROWN);
+				gameBoard.add(pp, 0, 7);
+				
+		// seventh  row
+			Rectangle qq = new Rectangle();
+				qq.setWidth(FIELD_SIZE);
+				qq.setHeight(FIELD_SIZE);
+				qq.setFill(Color.RED);
+				gameBoard.add(qq, 7, 6);
+					
+				Rectangle rr = new Rectangle();
+				rr.setWidth(FIELD_SIZE);
+				rr.setHeight(FIELD_SIZE);
+				rr.setFill(Color.ORANGE);
+				gameBoard.add(rr, 6, 6);
+				
+				Rectangle ss = new Rectangle();
+				ss.setWidth(FIELD_SIZE);
+				ss.setHeight(FIELD_SIZE);
+				ss.setFill(Color.PINK);
+				gameBoard.add(ss, 5, 6);
+				
+				Rectangle tt = new Rectangle();
+				tt.setWidth(FIELD_SIZE);
+				tt.setHeight(FIELD_SIZE);
+				tt.setFill(Color.GREEN);
+				gameBoard.add(tt, 4, 6);
+				
+				Rectangle uu = new Rectangle();
+				uu.setWidth(FIELD_SIZE);
+				uu.setHeight(FIELD_SIZE);
+				uu.setFill(Color.BLUE);
+				gameBoard.add(uu, 3, 6);
+				
+				
+				Rectangle vv = new Rectangle();
+				vv.setWidth(FIELD_SIZE);
+				vv.setHeight(FIELD_SIZE);
+				vv.setFill(Color.YELLOW);
+				gameBoard.add(vv, 2, 6);
+						
+				Rectangle ww = new Rectangle();
+				ww.setWidth(FIELD_SIZE);
+				ww.setHeight(FIELD_SIZE);
+				ww.setFill(Color.BROWN);
+				gameBoard.add(ww, 1, 6);
+				
+				Rectangle xx = new Rectangle();
+				xx.setWidth(FIELD_SIZE);
+				xx.setHeight(FIELD_SIZE);
+				xx.setFill(Color.PURPLE);
+				gameBoard.add(xx, 0, 6);
+					
+		// sixth
+				Rectangle aaa = new Rectangle();
+				aaa.setWidth(FIELD_SIZE);
+				aaa.setHeight(FIELD_SIZE);
+				aaa.setFill(Color.GREEN);
+				gameBoard.add(aaa, 7, 5);
+					
+				Rectangle bbb = new Rectangle();
+				bbb.setWidth(FIELD_SIZE);
+				bbb.setHeight(FIELD_SIZE);
+				bbb.setFill(Color.PINK);
+				gameBoard.add(bbb, 6, 5);
+				
+				Rectangle ccc = new Rectangle();
+				ccc.setWidth(FIELD_SIZE);
+				ccc.setHeight(FIELD_SIZE);
+				ccc.setFill(Color.ORANGE);
+				gameBoard.add(ccc, 5, 5);
+				
+				Rectangle ddd = new Rectangle();
+				ddd.setWidth(FIELD_SIZE);
+				ddd.setHeight(FIELD_SIZE);
+				ddd.setFill(Color.RED);
+				gameBoard.add(ddd,4, 5);
+				
+				Rectangle eee = new Rectangle();
+				eee.setWidth(FIELD_SIZE);
+				eee.setHeight(FIELD_SIZE);
+				eee.setFill(Color.PURPLE);
+				gameBoard.add(eee, 3, 5);
+				
+				
+				Rectangle fff = new Rectangle();
+				fff.setWidth(FIELD_SIZE);
+				fff.setHeight(FIELD_SIZE);
+				fff.setFill(Color.BROWN);
+				gameBoard.add(fff, 2, 5);
+						
+				Rectangle ggg = new Rectangle();
+				ggg.setWidth(FIELD_SIZE);
+				ggg.setHeight(FIELD_SIZE);
+				ggg.setFill(Color.YELLOW);
+				gameBoard.add(ggg, 1, 5);
+				
+				Rectangle hhh = new Rectangle();
+				hhh.setWidth(FIELD_SIZE);
+				hhh.setHeight(FIELD_SIZE);
+				hhh.setFill(Color.BLUE);
+				gameBoard.add(hhh, 0, 5);
+					
+		//fifth
+				
+				Rectangle iii = new Rectangle();
+				iii.setWidth(FIELD_SIZE);
+				iii.setHeight(FIELD_SIZE);
+				iii.setFill(Color.PINK);
+				gameBoard.add(iii, 7, 4);
+					
+				Rectangle jjj = new Rectangle();
+				jjj.setWidth(FIELD_SIZE);
+				jjj.setHeight(FIELD_SIZE);
+				jjj.setFill(Color.PURPLE);
+				gameBoard.add(jjj, 6, 4);
+				
+				Rectangle kkk = new Rectangle();
+				kkk.setWidth(FIELD_SIZE);
+				kkk.setHeight(FIELD_SIZE);
+				kkk.setFill(Color.BLUE);
+				gameBoard.add(kkk, 5, 4);
+				
+				Rectangle lll = new Rectangle();
+				lll.setWidth(FIELD_SIZE);
+				lll.setHeight(FIELD_SIZE);
+				lll.setFill(Color.ORANGE);
+				gameBoard.add(lll, 4, 4);
+				
+				Rectangle mmm = new Rectangle();
+				mmm.setWidth(FIELD_SIZE);
+				mmm.setHeight(FIELD_SIZE);
+				mmm.setFill(Color.BROWN);
+				gameBoard.add(mmm, 3, 4);
+								
+				Rectangle nnn = new Rectangle();
+				nnn.setWidth(FIELD_SIZE);
+				nnn.setHeight(FIELD_SIZE);
+				nnn.setFill(Color.GREEN);
+				gameBoard.add(nnn, 2, 4);
+						
+				Rectangle ooo = new Rectangle();
+				ooo.setWidth(FIELD_SIZE);
+				ooo.setHeight(FIELD_SIZE);
+				ooo.setFill(Color.RED);
+				gameBoard.add(ooo, 1, 4);
+				
+				Rectangle ppp = new Rectangle();
+				ppp.setWidth(FIELD_SIZE);
+				ppp.setHeight(FIELD_SIZE);
+				ppp.setFill(Color.YELLOW);
+				gameBoard.add(ppp, 0, 4);
+				
+		
+		root.setCenter(gameBoard);
+		
 		Label lblBottom = new Label("Bottom");
 		root.setBottom(lblBottom);
 		Label lblLeft = new Label("Left");
@@ -57,7 +491,7 @@ public class Kamisado_Client_View {
 		stage.setTitle(" K A M I S A D O ");
 		
 	}
-	
+
 	public void firstMove(){
 		//implement the first move here
 		if(model.black) {
